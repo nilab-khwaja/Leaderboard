8f790ba9f6944a2fbc48fb6260a21008977fdd74
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-05-17 17:04:25 +0430

    remove linters error

--- src/index.js
+++ src/index.js
@@ -1,62 +1,57 @@
 import './style.css';
-const gameId ='lk96JlhTBDrWvcnLFAii';
+
+const gameId = 'lk96JlhTBDrWvcnLFAii';
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
-
-
 
 // fetch(url, {
 //   method: 'POST',
 //   headers: {
 //     'Content-Type': 'application/json'
-//   }, 
+//   },
 //   body: JSON.stringify({ name: 'My cool new game' })
 // })
 // .then(response => response.json())
 // .then(data => console.log(data.result));
 
-
 // const scoreItem = document.getElementById('display-scores');
 // const form = document.getElementById('form');
 // const refresh = document.getElementById('refresh');
-
 
 const form = document.getElementById('form');
 const refresh = document.getElementById('refresh');
 
 form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-      const result = await response.json();
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  });
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // refresh btn
 
-
 refresh.addEventListener('click', async () => {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const scoresList = document.getElementById('display-scores');
-      data.result.forEach(score => {
-        const listItem = document.createElement('li');
-        listItem.innerText = `${score.user}: ${score.score}`;
-        scoresList.appendChild(listItem);
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  });
-
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const scoresList = document.getElementById('display-scores');
+    data.result.forEach((score) => {
+      const listItem = document.createElement('li');
+      listItem.innerText = `${score.user}: ${score.score}`;
+      scoresList.appendChild(listItem);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
